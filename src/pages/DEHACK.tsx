e7c655b161999ba5ef02f21cfddc4e6aec3ac5dc
aaru-sheep <f20251528@pilani.bits-pilani.ac.in>
2026-02-01 09:20:43 +0000

    "dehack regs internal"

--- src/pages/DEHACK.tsx
+++ src/pages/DEHACK.tsx
@@ -29,15 +29,7 @@ const Dehack = () => {
   const [teamMembers, setTeamMembers] = useState([]);
 
   const handleOpenModal = () => {
-    if (!isAuthenticated) {
-      toast.error('Please login first', {
-        description: 'You need to be logged in to register for events.'
-      });
-      return;
-    }
-    setCaptainName(user?.name || '');
-    setCaptainEmail(user?.email || '');
-    setShowModal(true);
+    window.open('https://docs.google.com/forms/d/e/1FAIpQLSfchPf5RNBt0-ymzQjWUTv5g7Owcl2OqxQGZyGOXOrpkXj3-Q/viewform', '_blank');
   };
 
   const handleNumMembersChange = e => {
